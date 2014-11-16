5f8ceb3af647aca5271ebe9b77f354e9430cbc48
Kawsik <roykawsik@gmail.com>
2014-11-16 22:17:56 +0600

    Practicing loop
    Today I practiced something about loop of PHP.

--- loops/index.php
+++ loops/index.php
@@ -1,18 +1,31 @@
 <?php 
 
-// $arr = ['AAA', 'BBB', 'CCC', 'DDD', 'EEE', 'FFF', 'GGG'];
-$arr = array(
-	'ceo'           => 'Koushik',
-	'manager'       => 'Tash',
-	'instrustor'    => 'Mohon'
-);
+$arr = array('Fruit', 'Mango', 'Sweet', 'Juicy'); 
 
 // foreach ($arr as $name) {
 // 	echo $name;
 // }
 
-foreach ($arr as $title => $name) {
-	echo "<li><strong>$title</strong> - $name</li>";
+// $arr = array(
+// 	'Father' 	=>	'Abba',
+// 	'Mother' 	=>	'Amma',
+// 	'Sister' 	=>	'Bon',
+// 	'Brother' 	=>	'Vai'
+// );
+
+// foreach ($arr as $title => $name) {
+// 	echo  "<li><b>$title</b> - $name</li>";
+// }
+
+// for ( $i=0; $i < count($arr); $i++ ) { 
+// 	echo "<li>$arr[$i]</li>";
+// }  
+
+$i = 0;
+while ($i < 2) {
+	echo "<li>$arr[$i]</li>";
 }
+
+
 
 ?>
